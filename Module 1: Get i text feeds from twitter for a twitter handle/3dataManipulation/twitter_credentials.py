554e5f7bd98e8da87966019b263533d8244b6501
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-11 13:24:43 -0500

    Create twitter_credentials.py

---
+++ Module 1: Get i text feeds from twitter for a twitter handle/3dataManipulation/twitter_credentials.py
@@ -0,0 +1,7 @@
+# copy yours in keys & token, once you need to use twitter API
+# If necessary, don't forget regenerating the keys and secrets
+
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
