2f0d269b6b11830a5478e1433fd79002bacd5295
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-11 13:25:00 -0500

    Update twitter_credentials.py

--- Module 1: Get i text feeds from twitter for a twitter handle/1Stream/twitter_credentials.py
+++ Module 1: Get i text feeds from twitter for a twitter handle/1Stream/twitter_credentials.py
@@ -1,4 +1,6 @@
-#copy yours in keys & token
+# copy yours in keys & token, once you need to use twitter API
+# If necessary, don't forget regenerating the keys and secrets
+
 ACCESS_TOKEN = ""
 ACCESS_TOKEN_SECRET = ""
 CONSUMER_KEY = ""
