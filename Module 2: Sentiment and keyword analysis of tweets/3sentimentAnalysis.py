b62503c52ce0fa38062c2ba418380a7479690aa5
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-11 10:22:18 -0500

    Update 3sentimentAnalysis.py

--- Module 2: Sentiment and keyword analysis of tweets/3sentimentAnalysis.py
+++ Module 2: Sentiment and keyword analysis of tweets/3sentimentAnalysis.py
@@ -4,26 +4,29 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
+
 def print_result(annotations):
     score = annotations.document_sentiment.score
     magnitude = annotations.document_sentiment.magnitude
-
+    file=open('sentiment.txt','w',encoding="utf-8")
     for index, sentence in enumerate(annotations.sentences):
         sentence_sentiment = sentence.sentiment.score
-        print('Sentence {} has a sentiment score of {}'.format(
-            index, sentence_sentiment))
-
+        file.write('Sentence {} has a sentiment score of {}'.format(
+            index, sentence_sentiment)+'\n')
+    file.close()
+    print('Sentiments written to file.')
     print('Overall Sentiment: score of {} with magnitude of {}'.format(
         score, magnitude))
     return 0
 
-def analyze(tweet):
+
+def analyze(tweets):
     """Run a sentiment analysis request on text within a passed filename."""
-    client = language.LanguageServiceClient.from_service_account_json("/media/sf_shared_folder/creds.json")
+    client = language.LanguageServiceClient()
 
-    with open("path to tweet_file") as tweet_file:
-
-        content = tweet_file.read()
+    with open('tweets.txt', 'r',encoding="utf-8") as review_file:
+        # Instantiates a plain text document.
+        content = review_file.read()
 
     document = types.Document(
         content=content,
@@ -33,5 +36,13 @@ def analyze(tweet):
     # Print the results
     print_result(annotations)
 
+
 if __name__ == '__main__':
-          analyze("path to tweet_file")
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'tweets',
+        help='The filename of the tweets file you\'d like to analyze.')
+    args = parser.parse_args()
+    analyze(args.tweets)
