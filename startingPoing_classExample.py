ef4176f75d9e76d06e71b2aedcef0b8ff982e490
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-11 13:28:18 -0500

    Update and rename class example to startingPoing_classExample.py

---
+++ startingPoing_classExample.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Author - Prateek Mehta // this is a class example
+
+import tweepy  # https://github.com/tweepy/tweepy
+import json5
+
+# Twitter API credentials
+consumer_key = "1172881526079533056-i2wWWCmkeDxiZQ10GeAT12JdPeq62E"
+consumer_secret = "lKbOhXc0Tsg2ale7ePoo9tDfzjTt8VZrAr9pjSCSCQukL"
+access_key = "1172881526079533056-i2wWWCmkeDxiZQ10GeAT12JdPeq62E"
+access_secret = "u33MCiQOb63vMQlhrrf9b7AOESttyQFkS3TfARmz9oM9lXIcsQ"
+
+def get_all_tweets(screen_name):
+    # Twitter only allows access to a users most recent 3240 tweets with this method
+
+    # authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    # initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    # make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name=screen_name, count=10)
+    # screen_name – Specifies the screen name of the user.
+    # Helpful for disambiguating when a valid screen name is also a user ID.
+
+    # save most recent tweets
+    alltweets.extend(new_tweets)
+
+    # save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    # keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+
+        # all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name=screen_name, count=10, max_id=oldest)
+
+        # save most recent tweets
+        alltweets.extend(new_tweets)
+
+        # update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        if (len(alltweets) > 15):
+            break
+        print
+        "...%s tweets downloaded so far" % (len(alltweets))
+
+    # write tweet objects to JSON
+    file = open('tweet.json', 'w')
+    print
+    "Writing tweet objects to JSON please wait..."
+    for status in alltweets:
+        json.dump(status._json, file, sort_keys=True, indent=4)
+
+    # close the file
+    print
+    "Done"
+    file.close()
+
+if __name__ == '__main__':
+    # pass in the username of the account you want to download
+    get_all_tweets("@Ibra_official")
