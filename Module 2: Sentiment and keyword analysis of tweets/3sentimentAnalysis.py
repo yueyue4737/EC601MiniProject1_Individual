e1252971b7455a9de1ce20b9382c121c4b4b906b
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-11 10:16:53 -0500

    Update 3sentimentAnalysis.py

--- Module 2: Sentiment and keyword analysis of tweets/3sentimentAnalysis.py
+++ Module 2: Sentiment and keyword analysis of tweets/3sentimentAnalysis.py
@@ -1,1 +1,37 @@
-sentimentAnalysis
+import argparse
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print('Sentence {} has a sentiment score of {}'.format(
+            index, sentence_sentiment))
+
+    print('Overall Sentiment: score of {} with magnitude of {}'.format(
+        score, magnitude))
+    return 0
+
+def analyze(tweet):
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language.LanguageServiceClient.from_service_account_json("/media/sf_shared_folder/creds.json")
+
+    with open("path to tweet_file") as tweet_file:
+
+        content = tweet_file.read()
+
+    document = types.Document(
+        content=content,
+        type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+
+    # Print the results
+    print_result(annotations)
+
+if __name__ == '__main__':
+          analyze("path to tweet_file")
