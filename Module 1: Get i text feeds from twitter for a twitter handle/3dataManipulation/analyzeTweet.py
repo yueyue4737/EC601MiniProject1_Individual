bd215ce5f6bcbfbbc8de9b940f22e60576e467c1
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-12 21:29:53 -0500

    Update analyzeTweet.py

--- Module 1: Get i text feeds from twitter for a twitter handle/3dataManipulation/analyzeTweet.py
+++ Module 1: Get i text feeds from twitter for a twitter handle/3dataManipulation/analyzeTweet.py
@@ -1,1 +1,17 @@
-dfd
+import csv
+
+with open("tweetV.csv") as cfile1:
+  reader = csv.DictReader(cfile1)
+  f = open("tweetV.txt", "w")
+  for row in reader:
+    if row.get("Tweets") != None:
+      print row.get("Tweets")
+      f.write(row.get("Tweets"))
+      
+with open("tweetE.csv") as cfile2:
+  reader = csv.DictReader(cfile2)
+  f = open("tweetE.txt", "w")
+  for row in reader:
+    if row.get("Tweets") != None:
+      print row.get("Tweets")
+      f.write(row.get("Tweets"))
