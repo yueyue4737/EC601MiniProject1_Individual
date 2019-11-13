ea3d88d519cb0d0f96b55fbd2a562b1f69073dfd
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-11-12 23:52:58 -0500

    Update 3Gnlp_sentimentAnalysis.py

--- Module 2: Sentiment and keyword analysis of tweets/3Gnlp_sentimentAnalysis.py
+++ Module 2: Sentiment and keyword analysis of tweets/3Gnlp_sentimentAnalysis.py
@@ -1,48 +1,75 @@
-import argparse
-
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import os
+from google.oauth2 import service_account
 
+credentials_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + "[FILENAME].json"
+credentials = service_account.Credentials.from_service_account_file(credentials_path)
 
-def print_result(annotations):
-    score = annotations.document_sentiment.score
-    magnitude = annotations.document_sentiment.magnitude
-    file=open('sentiment.txt','w',encoding="utf-8")
-    for index, sentence in enumerate(annotations.sentences):
-        sentence_sentiment = sentence.sentiment.score
-        file.write('Sentence {} has a sentiment score of {}'.format(
-            index, sentence_sentiment)+'\n')
-    file.close()
-    print('Sentiments written to file.')
-    print('Overall Sentiment: score of {} with magnitude of {}'.format(
-        score, magnitude))
-    return 0
+from google.cloud import language
 
+"""Analyze file with Google NLP
+Returns:
+    string -- Reaction/Sentiment of the text.
+"""
+def analyze_text(file_to_analyze):
+    # Instantiates a client
+    client = language.LanguageServiceClient(credentials=credentials)
 
-def analyze(tweets):
-    """Run a sentiment analysis request on text within a passed filename."""
-    client = language.LanguageServiceClient()
-
-    with open('tweets.txt', 'r',encoding="utf-8") as review_file:
-        # Instantiates a plain text document.
-        content = review_file.read()
+    # The text to analyze
+    with open(file_to_analyze, 'r') as f:
+            # Instantiates a plain text document.
+            content = f.read()
 
     document = types.Document(
         content=content,
         type=enums.Document.Type.PLAIN_TEXT)
-    annotations = client.analyze_sentiment(document=document)
 
-    # Print the results
-    print_result(annotations)
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
 
+    print('Text: {}'.format(content))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(
-        'tweets',
-        help='The filename of the tweets file you\'d like to analyze.')
-    args = parser.parse_args()
-    analyze(args.tweets)
+    reaction = get_reaction(sentiment.score, sentiment.magnitude)
+    print(reaction)
+
+"""Get the Reaction based off score and magnitude
+score: float, value from -1(Negative) to 1 (Positive)
+magnitude: float, value from 0 to +inf, indicating strength of score
+Returns:
+    string: reaction of text
+"""
+def get_reaction(score, magnitude):
+    reaction = 'Neutral'
+    if score >= 0.3:
+        if magnitude >= 3:
+            reaction = 'Clearly Positive'
+        elif 1 <= magnitude < 3:
+            reaction = 'Positive'
+        else:
+            reaction = 'Slightly Positive'
+
+    elif score <= -0.3:
+        if magnitude >= 3:
+            reaction = 'Clearly Negative'
+        elif 1 <= magnitude < 3:
+            reaction = 'Negative'
+        else:
+            reaction = 'Slightly Negative'
+
+    elif -.3 < score < .3:
+        if magnitude >= 3:
+            reaction = 'Mixed'
+        else:
+            reaction = 'Neutral'
+
+    return reaction
+
+if __name__ == "__main__":›
+    #select one at a time
+    file_to_analyze = 'tweetsV.txt'
+    #file_to_analyze = 'tweetsE.txt'
+
+    analyze_text(file_to_analyze)
