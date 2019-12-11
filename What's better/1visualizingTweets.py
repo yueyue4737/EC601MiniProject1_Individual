61ddc47cdb6572684a790e0d26a76fcff7a06e59
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-11 08:26:59 -0500

    Update 1visualizingTweets.py

--- What's better/1visualizingTweets.py
+++ What's better/1visualizingTweets.py
@@ -1,6 +1,7 @@
 # twitter_credentials.py(this file is used in each steps)
 # Variables that contains the user credentials to access Twitter API (save in the same directory)
 # Store in strings, fill in these before testing
+# This code is fixed in MiniProject3
 
 from tweepy import API 
 from tweepy import Cursor
